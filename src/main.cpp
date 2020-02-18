ff377ebb562e819f9c7dcb6397b57f851690ddb2
karasik <rosticus@rambler.ru>
2020-02-18 08:40:43 +0300

    Исправил ошибки (class Program, *Parser)

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,47 @@
 #include "parser.h"
 #include "expression.h"
 
+class Program {
+public:
+    void execute() {
+
+        Parser parser;
+
+        while(true) {
+            std::cout << "Введите выражение, или exit для выхода из приложения." << std::endl;
+            std::cout << "Ввод: ";
+            std::string str;
+            std::getline(std::cin, str);
+
+            if(str == "exit") break;
+
+            auto expr = parser.parse( str );
+
+            if( !expr ) {
+                std::cout << "Error!" << std::endl;
+                continue;
+            }
+
+            try {
+                double ans = expr->calculate();
+                std::cout << "Answer: " << ans << std::endl;
+            } catch(const char *e) {
+                std::cout << "Error, function not found!" << std::endl;
+                
+            } 
+
+            std::cout << std::endl;
+
+            delete expr;
+        } // while(true)
+    } // execute
+};
+
+
 int main() {
-    auto parser = new Parser;
+    Program program;
 
-    while(true) {
-        std::cout << "Введите выражение, или exit для выхода из приложения." << std::endl;
-        std::cout << "Ввод: ";
-        std::string str;
-        std::getline(std::cin, str);
-
-        if(str == "exit") break;
-
-        auto expr = parser->parse( str );
-
-        if( !expr ) {
-            std::cout << "Error!" << std::endl;
-            continue;
-        }
-
-        try {
-            double ans = expr->calculate();
-            std::cout << "Answer: " << ans << std::endl;
-        } catch(const char *e) {
-            std::cout << "Error, function not found!" << std::endl;
-            
-        } 
-
-        std::cout << std::endl;
-
-        delete expr;
-    }
-
-    delete parser;
+    program.execute();
 
     return 0;
 }
